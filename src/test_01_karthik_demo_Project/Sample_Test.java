1619c23aa68f492833b6b57707aff5f129f5e879
G Karthik <karthikchowdary020@gmail.com>
2023-04-03 17:34:07 +0530

    Test in  Github on 03/04/23.

---
+++ src/test_01_karthik_demo_Project/Sample_Test.java
@@ -0,0 +1,10 @@
+package test_01_karthik_demo_Project;
+
+public class Sample_Test {
+
+	public static void main(String[] args) {
+          System.out.println("Hello sir....!");;
+          System.out.println("This is Karthik........!");
+	}
+
+}
